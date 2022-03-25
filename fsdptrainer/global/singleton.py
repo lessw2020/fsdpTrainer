440346dddd658acd1fce3a3dd0d3d5312f574eec
lessw2020 <lessw@etrillium.com>
2022-03-25 10:34:29 -0700

    singleton stackoverflow reference

--- fsdptrainer/global/singleton.py
+++ fsdptrainer/global/singleton.py
@@ -1,5 +1,6 @@
 # Singleton class using metaclass
 # keeps a unique dict for global instances
+# https://stackoverflow.com/questions/6760685/creating-a-singleton-in-python#6798042
 
 class Singleton(type):
     instances = {}
